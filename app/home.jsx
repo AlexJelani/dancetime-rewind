e300d2d0818453a6e8794a82815d795a0e86b58f
JAlexander <jelanialexander@gmail.com>
2024-01-11 11:19:49 +0900

    updated homepage text

--- app/home.jsx
+++ app/home.jsx
@@ -15,33 +15,21 @@ export default function Home() {
             {/* punchilne and avatar */}
             <View className="flex-row justify-between items-center mx-5">
                 <View className="space-y-2">
-                    <Text
-                        style={{fontSize: hp(4.5)}}
-                        className="font-bold tracking-wider text-neutral-700"
-                    >
-                        READY TO
-                    </Text>
+
                     <Text
                         style={{fontSize: hp(4.5)}}
                         className="font-bold tracking-wider text-rose-500"
                     >
-                        DANCE
+                        <Text
+                            style={{fontSize: hp(4.5)}}
+                            className="font-bold tracking-wider text-neutral-700"
+                        >
+                            READY TO
+                        </Text> DANCE
                     </Text>
                 </View>
 
-                <View className="flex justify-center items-center space-y-2">
-                    <Image
-                        source={require('../assets/images/avatar.png')}
-                        style={{height: hp(6), width: hp(6)}}
-                        className="rounded-full"
-                    />
-                    <View
-                        className="bg-neutral-200 rounded-full flex justify-center items-center border-[3px] border-neutral-300"
-                        style={{height: hp(5.5), width: hp(5.5)}}
-                    >
-                        <Ionicons name="notifications" size={hp(3)} color="gray" />
-                    </View>
-                </View>
+
             </View>
 
             {/* image slider */}
