a91aa504a894270f01ded5fce38297abdd209576
JAlexander <jelanialexander@gmail.com>
2024-01-28 22:07:23 +0900

    added the dance detail card

--- components/DanceList.jsx
+++ components/DanceList.jsx
@@ -24,34 +24,31 @@ export default function DanceList({ data}) {
     );
 }
 
-const DanceCard = ({ item, index, router}) => {
-
+const DanceCard = ({ item, index, router }) => {
     return (
         <Animated.View entering={FadeInDown.duration(400).delay(index * 200).springify()}>
             <TouchableOpacity
                 onPress={() => router.push({ pathname: '/exerciseDetails', params: item })}
-                className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+                className="flex-row items-center justify-between p-4 border border-gray-500 rounded-lg mb-2"
             >
-                {/* Replace 'imageSource' with the actual image source */}
-                {/*<Image source={imageSource} style="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" />*/}
-
-                <View className="flex flex-col justify-between p-4 leading-normal">
-                    <Text className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <View className="flex-1">
+                    <Text className="text-2xl font-bold mb-2">
                         {item?.name?.length > 20 ? item.name.slice(0, 20) + '...' : item.name}
                     </Text>
-                    {/* Dummy subtext */}
-                    <Text className="mb-3 font-normal text-gray-600 dark:text-gray-400">7 steps</Text>
-                    {/* Add any other text or components you want here */}
+
+                    <View className="flex-row items-center">
+                        <View className="rounded-full bg-blue-500 p-2 mr-2">
+                            <Text className="text-white">7 steps</Text>
+                        </View>
+                    </View>
                 </View>
-                <View>
-                    {/* Play Icon Button */}
-                    <TouchableOpacity className="bg-blue-500 p-2 rounded-full">
-                        <FontAwesome name="play-circle" size={30} color="#fff" />
-                    </TouchableOpacity>
-                </View>
+
+                <TouchableOpacity  onPress={() => router.push({ pathname: '/exerciseDetails', params: item })} className="bg-blue-500 p-2 rounded-full">
+                    <FontAwesome name="play-circle" size={30} color="#fff" />
+                </TouchableOpacity>
             </TouchableOpacity>
         </Animated.View>
     );
-}
+};
 
 
