ebc94c240f838ebfe0370b0021b67d97f5632679
JAlexander <jelanialexander@gmail.com>
2024-02-05 15:49:13 +0900

    updated the exerciseDetails.jsx

--- app/exerciseDetails.jsx
+++ app/exerciseDetails.jsx
@@ -1,84 +1,94 @@
-import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
-import {useLocalSearchParams, useRouter} from "expo-router";
-import {Image as ExpoImage} from "expo-image/build/Image";
-import {heightPercentageToDP as hp, widthPercentageToDP as wp} from "react-native-responsive-screen";
-import AntiDesign from 'react-native-vector-icons/AntDesign'
-import {ScrollView} from "react-native-virtualized-view";
-
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import React from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Image } from 'expo-image';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Anticons from 'react-native-vector-icons/AntDesign';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+//TODO safeareaview
 export default function exerciseDetails() {
     const item = useLocalSearchParams();
     const router = useRouter();
-    console.log('got item', item);
-    const {name} = item;
     return (
         <View className="flex flex-1">
-            <View>
-                <TouchableOpacity onPress={()=>router.back()}>
-                    <ExpoImage
-                        source={{ uri: item.gifUrl }}
-                        contentFit='cover'
-                        style={{width: wp(100), height: wp(100)}}
-                        className="rounded-[40px]"
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity
-                onPress={()=> router.back()}
-                 className="mx-2 absolute rounded-full mt-10 right-0">
-                    <AntiDesign name="closecircle" size={hp(4.5)} colors="#f43f5e"/>
-                </TouchableOpacity>
-
-                {/*details */}
-                <ScrollView className="mx-4 space-y-2 mt-3">
-                    <Text style={{fontSize:hp(3.5)}}
-                            className='font-semibold text-neutral-800 tracking-wide'>
-                        {name}
-                    </Text>
-                    <Text style={{fontSize:hp(2)}}
-                          className='font-bold text-neutral-800 '>
-                        Equipment {item?.equipment}
-                    </Text>
-                </ScrollView>
+            <View className="shadow-md bg-white rounded-b-[40px]">
+                <Image
+                    source={{uri: item.gifUrl}}
+                    contentFit='cover'
+                    style={{width:wp(100), height: wp(100) }}
+                    className="rounded-b-[40px]"
+                />
             </View>
 
+            <TouchableOpacity
+                onPress={()=> router.back()}
+                className="mx-2 absolute rounded-full mt-2 right-0"
+            >
+                <Anticons name="closecircle" size={hp(4.5)} color="#f43f5e" />
+            </TouchableOpacity>
+
+            {/* details */}
+
+            <ScrollView className="mx-4 space-y-2 mt-3" showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 60}}>
+                <Animated.Text
+                    entering={FadeInDown.duration(300).springify()}
+                    style={{fontSize: hp(3.5)}}
+                    className="font-semibold text-neutral-800 tracking-wide"
+                >
+                    {item.name}
+                </Animated.Text>
+                <Animated.Text
+                    entering={FadeInDown.delay(100).duration(300).springify()}
+                    style={{fontSize: hp(2)}}
+                    className=" text-neutral-700 tracking-wide"
+                >
+                    Equipment <Text className="font-bold text-neutral-800">
+                    {item?.equipment}
+                </Text>
+                </Animated.Text>
+                <Animated.Text
+                    entering={FadeInDown.delay(200).duration(300).springify()}
+                    style={{fontSize: hp(2)}}
+                    className=" text-neutral-700 tracking-wide"
+                >
+                    Secondary Muscles <Text className="font-bold text-neutral-800">
+                    {item?.secondaryMuscles}
+                </Text>
+                </Animated.Text>
+                <Animated.Text
+                    entering={FadeInDown.delay(300).duration(300).springify()}
+                    style={{fontSize: hp(2)}}
+                    className=" text-neutral-700 tracking-wide"
+                >
+                    Target <Text className="font-bold text-neutral-800">
+                    {item?.target}
+                </Text>
+                </Animated.Text>
+
+                <Animated.Text
+                    entering={FadeInDown.delay(400).duration(300).springify()}
+                    style={{fontSize: hp(3)}}
+                    className="font-semibold text-neutral-800 tracking-wide"
+                >
+                    Instructions
+                </Animated.Text>
+
+                {
+                    item.instructions?.split(',').map((instruction, index)=>{
+                        return (
+                            <Animated.Text
+                                entering={FadeInDown.delay((index+5)*100).duration(300).springify()}
+                                key={instruction}
+                                style={{fontSize: hp(1.7)}}
+                                className="text-neutral-800"
+                            >
+                                {instruction}
+                            </Animated.Text>
+                        )
+                    })
+                }
+            </ScrollView>
+
         </View>
-
-    );
+    )
 }
-// import React, { useRef } from 'react';
-// import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-// import { useLocalSearchParams, useRouter } from 'expo-router';
-// import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
-//
-// export default function ExerciseDetails() {
-//     const item = useLocalSearchParams();
-//     const router = useRouter();
-//
-//     const bottomsheetRef = React.useRef(null)
-//     const snapPoints = ['40%'];
-//
-//     return (
-//         <View style={styles.container}>
-//             <TouchableOpacity onPress={() => router.back()}>
-//                 <Text>Go back</Text>
-//             </TouchableOpacity>
-//
-//             {/* Use BottomSheet component */}
-//             <BottomSheet ref={bottomsheetRef} snapPoints={snapPoints} enablePanDownToClose={true}>
-//                 <BottomSheetView>
-//                     <Text>Hello</Text>
-//                 </BottomSheetView>
-//             </BottomSheet>
-//         </View>
-//     );
-// }
-//
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: 'gray',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     },
-// });
-
